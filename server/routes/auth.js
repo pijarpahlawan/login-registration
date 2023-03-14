ed728b88ed854ee3705125d120cf8975baeded25
Pijar <pijarpahlawanq@students.amikom.ac.id>
2023-03-14 09:39:48 +0700

    create login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
+/* eslint-disable quotes */
 /* eslint-disable indent */
 /* eslint-disable object-curly-spacing */
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes, Op } = require('sequelize');
 const express = require('express');
 const bcrypt = require('bcrypt');
 const getModel = require('../models/user');
@@ -50,4 +51,37 @@ router.post('/register', async (req, res) => {
   }
 });
 
+// login route
+router.post('/login', async (req, res) => {
+  try {
+    //  1. destrukturisasi req.body
+    const { name, email, password } = req.body;
+
+    //  2. cek apakah user sudah terdaftar
+    const userFinded = await User.findOne({
+      where: {
+        [Op.or]: [{ userName: name }, { userEmail: email }],
+      },
+    });
+    if (Object.keys(userFinded).length === 0) {
+      return res.status(401).send("Email doesn't registered");
+    }
+
+    //  3. cek jika password yang dimasukkan cocok dengan passwor di database
+    const validPassword = await bcrypt.compare(
+      password,
+      userFinded.userPassword,
+    );
+    if (!validPassword) {
+      return res.status(401).send("Password doesn't match");
+    }
+
+    //  4. berikan jwt token
+    const token = jwtGenerator(userFinded.userId);
+    return res.status(200).json({ token });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send('server error');
+  }
+});
 module.exports = router;
